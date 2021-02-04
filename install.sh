3a134cad9216b44216936f97cfbcbf5cb7cef2c5
Nerux1337 <78462347+Nerux1337@users.noreply.github.com>
2021-02-04 06:26:56 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 clear
-echo "   ┕━━━━╗✹╔━━━━┙
+echo '   ┕━━━━╗✹╔━━━━┙
 echo '╔━═━═━︽︾♚︾︽━═━═━╗
-echo "     1 — Termux
+echo '     1 — Termux
 echo '     2 —  Unix
-echo "     3 —  iSH
+echo '     3 —  iSH
 echo '╚━═━═━︾︽♔︽︾━═━═━╝
 read numb
 if [ $numb = "1" ]
